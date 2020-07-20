30ca6e2ef97447d067b3bfd6dfba6bcd9bf19675
DhruvKinger <cse1785@gmail.com>
2020-07-20 17:02:53 +0530

    added files

---
+++ ProductionAPI/Controllers/DataController.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductionAPI.Controllers
+{
+    public class DataController : ApiController
+    {
+        public IEnumerable<User> Get()
+        {
+
+            using (ProductionAppEntities entities = new ProductionAppEntities())
+            {
+                return entities.Users.ToList();
+            }
+        }
+
+
+        public User Get(string Id)
+        {
+            using (ProductionAppEntities entities = new ProductionAppEntities())
+            {
+                return entities.Users.FirstOrDefault(r=>r.id == Id);
+            }
+        }
+
+    }
+}
